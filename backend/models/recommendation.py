47745f6ccb84efee53b901cbcab08ae6dfd58d47
evoosa <eva@scytale.ai>
2022-05-07 19:25:13 +0300

    started writing recoimmendation logic

--- backend/models/recommendation.py
+++ backend/models/recommendation.py
@@ -22,11 +22,24 @@ class recommendation():
         self.recommended_art_supplies = self.og_art_supplies.copy()
 
     def get_recommended_groups(self):
+        def _remove_supply(self, supply):
+            try:
+                self.recommended_art_supplies[supply].pop(supply)
+                self.logger.debug(f'removed {supply}')
+            except KeyError:
+                self.logger.debug(f'removing {supply} failed since it\'s missing')
+
         # if the supplies need to be compact, drop everything that is not compact. else, pass
         if self.answer['compact'] == '0':
             for supply in self.recommended_art_supplies.keys():
                 if supply not in self.groups['compact']:
-                    poped_supply = self.recommended_art_supplies[supply].popitem()
-                    self.logger(f'removed {poped_supply[0]} since it\'s not compact')
+                    self.logger.info(f'removing {supply} since it\'s not compact')
+                    _remove_supply(self, supply)
 
-        # if self.answer
+        # if she doesn't like experimenting, remove all supplies that she won't like. if she does, do nothing
+        if self.answer['likes_experimenting'] == '1':
+            for group in self.groups.keys():
+                if group not in self.answer['preffered_art_types']:
+                    for supply in self.groups[group]:
+                        self.logger.info(f'removing {supply} since the spouse doesn\'t like {group}')
+                        _remove_supply(self, supply)
