9418a946c40e3be16804d4db243baa179315f687
evoosa <eva@scytale.ai>
2022-05-24 19:20:20 +0300

    i think backend work ok yay

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, jsonify
 from flask_cors import CORS
 
 from models.recommendation import recommendation
 
 app = Flask(__name__)
-# CORS(app, resources={r"/get_recommended_art_supplies": {"origins": "*"}})
-# app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app, resources={r"/get_recommended_art_supplies": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 test_answer = {
     'compact': '0',  # indoors / outdoors / NM
@@ -18,14 +18,14 @@ test_answer = {
 
 
 @app.route('/get_recommended_art_supplies', methods=['GET', 'POST'])
-# @cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def get_recommended_art_supplies():
     if request.method == 'POST':
-        print(request)
+        response = jsonify({'some': 'data'})
+        response.headers.add('Access-Control-Allow-Origin', '*')
         # rec = recommendation(request.form)
         # rec.get_recommended_groups()
         # return {key: val.attributes for (key, val) in rec.recommended_art_supplies.items()}
 
 
 if __name__ == '__main__':
-    app.run('localhost', 6969)
+    app.run('0.0.0.0', 6969)
