6869542c44ef8e14271fb1268dc96d0061a88171
evoosa <eva@scytale.ai>
2022-05-27 16:16:52 +0300

    added text to the popup what a joy

--- frontend/src/Components/PopupButton.jsx
+++ frontend/src/Components/PopupButton.jsx
@@ -14,7 +14,7 @@ const Popup = props => {
 function PopupButton(props) {
     const {art_supply} = props
     const [isOpen, setIsOpen] = useState(false);
-
+    console.log(art_supply)
     const togglePopup = () => {
         setIsOpen(!isOpen);
     }
@@ -28,7 +28,38 @@ function PopupButton(props) {
         />
         {isOpen && <Popup
             content={<>
-                <p>{art_supply.description}</p>
+                <h2 style={{
+                    fontWeight: "bold",
+                    fontSize: "25px",
+                    color: "#c769b5"
+                }}>{art_supply.material_name}</h2>
+                <h2 className={"popup-header"}>description</h2>
+                <p>
+                    {art_supply.description}
+                </p>
+                <h2 className={"popup-header"}>prices</h2>
+                <p>
+                    starts from {art_supply.min_price_ils} ILS per {art_supply.price_per}
+                    {(art_supply.available_in_set === '0' ? ', it\'s available in sets' : '')}
+                    {(art_supply.available_in_singles === '0' ? ', it\'s available in singles.' : '')}
+                </p>
+                <h2 className={"popup-header"}>Availability</h2>
+                <p>
+                    in {art_supply.stores}
+                </p>
+                <h2 className={"popup-header"}>Experience Level</h2>
+                <p>
+                    {(art_supply.experience_level === '0' ? 'no previous experience is required' : '')}
+                    {(art_supply.experience_level === '1' ? 'requires a bit of experience, artistic orientation is more than enough' : '')}
+                    {(art_supply.experience_level === '2' ? 'previous experience is preferable' : '')}
+                </p>
+                <h2 className={"popup-header"}>Good To Know</h2>
+                <p>
+                    {(art_supply.compact === '0' ? 'it\'s compact and can be carried around' : '')}
+                    {(art_supply.messy === '0' ? 'beware that it\'s a bit of messy' : '')}
+                    {(art_supply.safe_bet === '0' ? 'it\'s a safe bet so you can\'t fuckup with this one' : '')}
+                    {(art_supply.together === '0' ? 'it can be enjoyed together which is nice' : '')}
+                </p>
             </>}
             handleClose={togglePopup}
         />}
